9d1d1bf90bd83aad2bbed6cc753fa07c7e6225a2
ksh4704 <82083571+ksh4704@users.noreply.github.com>
2021-11-28 13:11:45 +0900

    김희목 UI

--- app/src/main/java/com/example/helloandroid/login/LoginFragment.java
+++ app/src/main/java/com/example/helloandroid/login/LoginFragment.java
@@ -3,11 +3,20 @@ package com.example.helloandroid.login;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.navigation.Navigation;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 import com.example.helloandroid.R;
 
 /**
@@ -17,8 +26,13 @@ import com.example.helloandroid.R;
  */
 public class LoginFragment extends Fragment {
 
+    private FirebaseAuth mFirebaseAuth;     //파이어베이스 인증
+    private DatabaseReference mDatabaseRef; //실시간 데이터베이스
+    private EditText mEtEmail, metPwd;      //로그인 입력필드
+
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
 
@@ -56,11 +70,20 @@ public class LoginFragment extends Fragment {
             mParam2 = getArguments().getString(ARG_PARAM2);
         }
     }
-
+    //main_fragment_search_button
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_login, container, false);
+        View inflateView = inflater.inflate(R.layout.fragment_login, container, false);;
+
+        Button Register_Botton = inflateView.findViewById(R.id.btn_register3);
+        Register_Botton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                Navigation.findNavController(inflateView).navigate(R.id.action_loginFragment_to_registerFragment);
+            }
+        });
+
+        return inflateView;
     }
 }
